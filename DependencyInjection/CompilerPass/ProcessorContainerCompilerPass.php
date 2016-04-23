0f1e46cc804406727182b435ff4e5c809d69579d
Daan van den Berg <info@doppy.nl>
2016-04-23 19:08:39 +0200

    refactor compilers to baseclass insteead of trait

--- DependencyInjection/CompilerPass/ProcessorContainerCompilerPass.php
+++ DependencyInjection/CompilerPass/ProcessorContainerCompilerPass.php
@@ -2,16 +2,40 @@
 
 namespace Doppy\ShortcodeBundle\DependencyInjection\CompilerPass;
 
-use Doppy\UtilBundle\Helper\CompilerPass\TaggedServicesTrait;
+use Doppy\UtilBundle\Helper\CompilerPass\BaseTagServiceCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
 
-class ProcessorContainerCompilerPass implements CompilerPassInterface
+class ProcessorContainerCompilerPass extends BaseTagServiceCompilerPass
 {
-    use TaggedServicesTrait;
-
-    public function process(ContainerBuilder $container)
+    protected function handleTag(
+        ContainerBuilder $containerBuilder,
+        Definition $serviceDefinition,
+        Reference $taggedServiceReference,
+        $attributes
+    )
     {
-        $this->processTaggedServices($container, 'doppy_shortcode.processor', 'add', true, true);
+        $serviceDefinition->addMethodCall(
+            'add',
+            array(
+                $attributes['service_id']
+            )
+        );
+    }
+
+    protected function getService(ContainerBuilder $containerBuilder)
+    {
+        return $containerBuilder->getDefinition('doppy_shortcode.processor');
+    }
+
+    protected function getTaggedServices(ContainerBuilder $containerBuilder)
+    {
+        return $containerBuilder->findTaggedServiceIds('doppy_shortcode.processor');
+    }
+
+    protected function adjustTaggedService(Definition $definition)
+    {
+        $definition->setLazy(true);
     }
 }
--- DependencyInjection/CompilerPass/TagHandlerCompilerPass.php
+++ DependencyInjection/CompilerPass/TagHandlerCompilerPass.php
@@ -2,16 +2,35 @@
 
 namespace Doppy\ShortcodeBundle\DependencyInjection\CompilerPass;
 
-use Doppy\UtilBundle\Helper\CompilerPass\TaggedServicesTrait;
+use Doppy\UtilBundle\Helper\CompilerPass\BaseTagServiceCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
 
-class TagHandlerCompilerPass implements CompilerPassInterface
+class TagHandlerCompilerPass extends BaseTagServiceCompilerPass
 {
-    use TaggedServicesTrait;
-
-    public function process(ContainerBuilder $container)
+    protected function handleTag(
+        ContainerBuilder $containerBuilder,
+        Definition $serviceDefinition,
+        Reference $taggedServiceReference,
+        $attributes
+    )
     {
-        $this->processTaggedServices($container, 'doppy_shortcode.tag_handler', 'addHandler');
+        $serviceDefinition->addMethodCall(
+            'addHandler',
+            array(
+                $taggedServiceReference
+            )
+        );
+    }
+
+    protected function getService(ContainerBuilder $containerBuilder)
+    {
+        return $containerBuilder->getDefinition('doppy_shortcode.tag_handler');
+    }
+
+    protected function getTaggedServices(ContainerBuilder $containerBuilder)
+    {
+        return $containerBuilder->findTaggedServiceIds('doppy_shortcode.tag_handler');
     }
 }
